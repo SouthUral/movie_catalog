387283bbc5fa00b8f2474fa00e2b2d442ba63885
vladimir <vladimir1012kov@gmail.com>
2022-10-11 20:52:25 +0300

    all models are written

--- django_movie/movies/models.py
+++ django_movie/movies/models.py
@@ -1,15 +1,8 @@
-from distutils.command.upload import upload
-from email.policy import default
-from enum import unique
-from tabnanny import verbose
-from unicodedata import category
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from datetime import date
 
+
 # Create your models here.
-
-
 class Category(models.Model):
     name = models.CharField("Категория", max_length=150)
     description = models.TextField("Описание")
@@ -81,6 +74,57 @@ class Movie(models.Model):
         verbose_name = "Фильм"
         verbose_name_plural = "Фильмы"
 
-    
 
-    
+
+class MovieShots(models.Model):
+    title = models.CharField("", max_length=100)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="movie_shots/")
+    movie = models.ForeignKey(Movie, verbose_name="Фильм", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Кадр из фильма"
+        verbose_name_plural = "Кадры из фильма"
+
+
+class RatingStar(models.Model):
+    value = models.SmallIntegerField("Значение", default=0)
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = "Звезда рейтинга"
+        verbose_name_plural = "Звезды рейтинга"
+
+
+class Rating(models.Model):
+    ip = models.CharField("IP адрес", max_length=15)
+    star = models.ForeignKey(RatingStar, verbose_name="звезда", on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, verbose_name="Фильм", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.star} - {self.movie}"
+
+    class Meta:
+        verbose_name = "Рейтинг"
+        verbose_name_plural = "Рейтинги"
+
+class Reviews(models.Model):
+    email = models.EmailField()
+    name = models.CharField("Имя", max_length=100)
+    text = models.TextField("Сообщение", max_length=5000)
+    parent = models.ForeignKey(
+        'self', verbose_name="Родитель", on_delete=models.SET_NULL, blank=True, null=True
+        )
+    movie = models.ForeignKey(Movie, verbose_name="Фильм", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.movie}"
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
